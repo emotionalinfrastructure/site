be9636b18af824f681f7495a30500236170a107a
Brittany Wright <brittanywright@emotionalinfrastructure.org>
2026-06-10 09:25:17 -0500

    Add site configuration placeholders

---
+++ lib/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "Emotional Infrastructure™",
+  domain: "https://emotionalinfrastructure.org",
+  founder: "Brittany Wright",
+  email: "brittanywright@emotionalinfrastructure.org",
+  creatorKitCheckoutUrl: "#checkout-creator-kit",
+  auditCheckoutUrl: "#checkout-audit",
+  newsletterUrl: "#newsletter",
+  contactFormUrl: "mailto:brittanywright@emotionalinfrastructure.org?subject=Emotional%20Infrastructure%20Inquiry"
+};
